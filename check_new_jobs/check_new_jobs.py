550bf07b54100ff2d2cfa39a8140fda0ae1dcdd9
david <davidola93@gmail.com>
2023-09-04 22:16:20 +0000

    update to current

--- check_new_jobs/check_new_jobs.py
+++ check_new_jobs/check_new_jobs.py
@@ -1,8 +1,6 @@
 import datetime, logging
-from sqlalchemy import select
 from .webcrawl import fetch_all_table_rows
-import check_new_jobs.database as db
-from check_new_jobs.table_models import Listing
+from . import database as db
 from .email import mail_new_listings, get_email_client, EmailFailed
 
 logger = logging.getLogger(__name__)
@@ -18,16 +16,18 @@ def main() -> None:
 
     email_client = get_email_client()
 
-    with (db.get_session(db.get_engine()) as session):
-        old_listings = session.scalars(select(Listing)).all()
+    with db.create_session(db.create_engine()) as session:
+        session_manager = db.SessionManager(session)
+        old_listings = session_manager.fetch_listings()
         old_listing_hrefs = [listing.href for listing in old_listings]
         new_listings = [
             listing for listing in all_current_listings if listing.href not in old_listing_hrefs
         ]
 
-        for user in db.get_users(session):
+        for user in session_manager.fetch_users():
             new_matching_listings = [
-                listing for listing in new_listings if keywords_in_string(db.get_keywords(session, user), listing.name)
+                listing for listing in new_listings if
+                keywords_in_string(session_manager.fetch_keywords(user), listing.name)
             ]
             if new_matching_listings:
                 try:
@@ -35,8 +35,12 @@ def main() -> None:
                 except EmailFailed as ex:
                     logger.critical("Failed to mail new listings to a user", exc_info=ex)
 
-        for listing in new_listings:
-            session.add(listing)
+        session_manager.insert_listings(new_listings)
+
+
+# TODO: Delete old listings.
+# Also: what if something else went wrong, might want to preserve them?
+# TODO: what if the same listing is reposted with a different href? seems to happen occasionally
 
 def keywords_in_string(keywords: list[str], string: str) -> bool:
     for keyword in keywords:
